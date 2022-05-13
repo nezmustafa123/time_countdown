9f6d367d0c9ae84a94df15d00b8e97aaa32dfdca
Nez Mustafa <nezmustafa123@gmail.com>
2022-05-13 21:20:14 +0100

    padding

--- script.js
+++ script.js
@@ -8,17 +8,23 @@ let counting = document.getElementById("countdown");
 
 function startCountdown() {
   let promoTimer = setInterval(() => {
-   if(promoTime <= 0) {
-  clearInterval(promoTimer); //clear interval
-  counting.innerHTML = 'Promo has ended';
-} else {
-   promoTime = promoTime - 1;
-   const days = Math.floor(promoTime / 3600 / 24); //3600 seconds in an hour
-   const hours = Math.floor(promoTime / 3600) % 24; //what's left after getting number of days
-   const min = Math.floor(promoTime / 60) % 60; //get minutes divide by 60
-   const sec = Math.floor(promoTime % 60);
-   counting.innerHTML = `TIME: ${hours}hr: ${min}min :${sec}`;
-   }
-  }. 1000)
+    if (promoTime <= 0) {
+      clearInterval(promoTimer); //clear interval
+      counting.innerHTML = "Promo has ended";
+    } else {
+      promoTime--;
+      const days = Math.floor(promoTime / 3600 / 24); //3600 seconds in an hour
+      const hours = Math.floor(promoTime / 3600) % 24; //what's left after getting number of days
+      const min = Math.floor(promoTime / 60) % 60; //get minutes divide by 60
+      const sec = Math.floor(promoTime % 60);
+      counting.innerHTML = `TIME: ${hours} hr: ${format(min)} min :${format(
+        sec
+      )}`;
+    }
+  }, 1000);
 }
 
+function format(t) {
+  return t < 10 ? `0${t}` : t;
+}
+startCountdown();
